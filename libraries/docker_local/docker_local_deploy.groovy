4ae09cd6ba078b63e82fe1749f5703445aa96f01
Brandon S <sheppard_brandon@bah.com>
2019-12-04 14:33:22 -0500

    updated repo URL

--- libraries/docker_local/docker_local_deploy.groovy
+++ libraries/docker_local/docker_local_deploy.groovy
@@ -1,7 +1,7 @@
 void call(app_env){
     stage "Deploy ${app_env.long_name} to Local Docker", {
         node{
-            sh "docker pull localhost:5000/sdp-labs-sample-app:${env.GIT_SHA}"
+            sh "docker pull 172.21.0.2:5000/sdp-labs-sample-app:${env.GIT_SHA}"
         }
     }
 }
