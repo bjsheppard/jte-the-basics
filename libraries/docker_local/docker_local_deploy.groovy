6cfa048cfe6e5dd86dc21224f7b883ed3ffd0b35
Brandon S <sheppard_brandon@bah.com>
2019-12-04 10:18:08 -0500

    added docker local library

---
+++ libraries/docker_local/docker_local_deploy.groovy
@@ -0,0 +1,7 @@
+def call() {
+    stage "Deploy Cotainer to Local Docker", {
+        sh "docker pull localhost:5000/sdp-labs-sample-app:${env.GIT_SHA}"
+    }
+}
+// docker pull localhost:5000/sdp-labs-sample-app:2f51ad978a5db23dd191799b4ea92941dd7a8c0e
+// docker run -d -p 8081:8080 localhost:5000/sdp-labs-sample-app:2f51ad978a5db23dd191799b4ea92941dd7a8c0e
