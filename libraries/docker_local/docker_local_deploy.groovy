b4e218252b9cbd517e76ed8dfbe65145fae4f018
Brandon S <sheppard_brandon@bah.com>
2019-12-04 13:31:24 -0500

    removed comments

--- libraries/docker_local/docker_local_deploy.groovy
+++ libraries/docker_local/docker_local_deploy.groovy
@@ -1,7 +1,5 @@
-void call(app_env) {
+void call(app_env){
     stage "Deploy ${app_env.long_name} to Local Docker", {
         sh "docker pull localhost:5000/sdp-labs-sample-app:${env.GIT_SHA}"
     }
 }
-// docker pull localhost:5000/sdp-labs-sample-app:2f51ad978a5db23dd191799b4ea92941dd7a8c0e
-// docker run -d -p 8081:8080 localhost:5000/sdp-labs-sample-app:2f51ad978a5db23dd191799b4ea92941dd7a8c0e
