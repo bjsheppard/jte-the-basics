3c94a550f279e20ca744c1d74fe29d5f9bb69d5f
Brandon S <sheppard_brandon@bah.com>
2019-12-09 13:47:37 -0500

    updated pipeline config file

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,4 +1,41 @@
 libraries{
-    gradle
-    bs_sonarqube
+  sdp{
+    images{
+      registry = "http://0.0.0.0:5000" // registry url
+      cred = "sdp-docker-registry"// jenkins cred id to authenticate
+      docker_args = "--network=try-it-out_sdp"  // docker runtime args
+    }
+  }
+  github{
+    source_type = "github"
+  }
+  sonarqube{
+    enforce_quality_gate = true
+  }
+  docker{
+    registry = "0.0.0.0:5000"
+    cred = "sdp-docker-registry"
+    remove_local_image = true
+  }
+  docker_local
+}
+
+stages{
+    continuous_integration{
+        static_code_analysis
+        build
+    }
+}
+
+application_environments{
+    dev{
+        long_name = "Development"
+        docker_network = "dev"
+        docker_prt = "8081"
+    }
+    prod{
+        long_name = "Production"
+        docker_network = "prod"
+        docker_port = "8082"
+    }
 }
